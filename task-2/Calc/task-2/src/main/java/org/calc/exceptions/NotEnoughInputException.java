1e2e169c9357da8b1391041324729424c1a113bc
IoanUshakov <i.ushakov@g.nsu.ru>
2026-02-28 21:53:39 +0700

    added new exception

---
+++ task-2/Calc/task-2/src/main/java/org/calc/exceptions/NotEnoughInputException.java
@@ -0,0 +1,7 @@
+package org.calc.exceptions;
+
+public class NotEnoughInputException extends CalculatorException {
+    public NotEnoughInputException(){
+        super("Not enough elements in input");
+    };
+}
