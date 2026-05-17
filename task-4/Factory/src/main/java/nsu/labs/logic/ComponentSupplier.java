3169ceb3bdf81c24d31ea48651286b3bc5c4f884
IoanUshakov <i.ushakov@g.nsu.ru>
2026-05-17 13:19:10 +0700

    added item logger

--- task-4/Factory/src/main/java/nsu/labs/logic/ComponentSupplier.java
+++ task-4/Factory/src/main/java/nsu/labs/logic/ComponentSupplier.java
@@ -3,8 +3,11 @@ package nsu.labs.logic;
 import nsu.labs.model.Entity;
 
 import nsu.labs.storage.Storage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ComponentSupplier<T extends Entity> extends Thread{
+    private static final Logger logger = LoggerFactory.getLogger(ComponentSupplier.class);
     private final Storage<T> storage;
     private final Class<T> compClass;
     private int delay;
@@ -31,6 +34,8 @@ public class ComponentSupplier<T extends Entity> extends Thread{
                     e.printStackTrace();
                 }
                 storage.put(item);
+                String supplierClassName = compClass.getSimpleName();
+                logger.debug("receive new {} from {} supplier, componentId: {} ", supplierClassName, Thread.currentThread().getId(), item.getId());
 
                 int currentDelay;
                 synchronized (this){
